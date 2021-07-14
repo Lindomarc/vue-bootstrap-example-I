ea53082965dcb19c954667c9fd91809c7fed57c3
lindomarc <lindomarc@outlook.com>
2021-07-13 22:46:15 -0300

    feat: toltip hover

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -1,29 +1,32 @@
 <template>
-    <div>
+  <div>
     <b-col>
-      <b-card 
-      :title="name" 
-      img-src="https://picsum.photos/600/300/?image=25"
-      img-alt="job-info"
-      img-top
-      tag="article"
-      style="max-width:20rem"
-      footer="Card Footer"
-      class="mb-2">
-        <b-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, aperiam!</b-card-text  >
-        <b-button variant="primary">Apply</b-button>
+      <b-card
+        :title="name"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="job-info"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        footer="Card Footer"
+        class="mb-2"
+      >
+        <b-card-text
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+          aperiam!</b-card-text
+        >
+        <b-button variant="primary" v-b-tooltip.hover title="Apply Here"
+          >Apply</b-button
+        >
       </b-card>
     </b-col>
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  props:["name"]
-    
-}
+  props: ["name"],
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
